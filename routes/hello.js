389045cb6a95544f450a1c21ceecdfef496194ff
Rahul Dutta <rdutta845@gmail.com>
2017-03-09 13:56:36 +0530

    .gitignore

---
+++ routes/hello.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+
+
+
+
+router.get('/', function(req, res, next) {
+  
+  res.send('Please provide a number');
+});
+
+
+
+
+module.exports = router;
+
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const Slapp = require('slapp')
+const BeepBoopConvoStore = require('slapp-convo-beepboop')
+const BeepBoopContext = require('slapp-context-beepboop')
+ if (!process.env.PORT) throw Error('PORT missing but required')
+
+ 
+ var slapp = Slapp({
+ 	convo_store : BeepBoopConvoStore(),
+ 	context : BeepBoopContext()
+ })	
+
+var app = slapp.attachToExpress(express())
+
+slapp.message('hi (.*)',['direct_message'], (msg, text, match1)=>{
+	msg.say('How r you').route('handleHi', {what : match1})
+})
+
+slapp.route('handleHi', (msg, state)=>{
+	msg.say(':smile ' + state.what)
+})
+
+app.get('/', function(req, res){
+	res.send('Hello World')
+})
+console.log('Listening on :' + process.env.PORT)
+
+app.listen(process.env.PORT)
